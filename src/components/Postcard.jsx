52deff8ffb2e273b29ffe4893a217a8928e47a34
Kushal Ghosh <kg2530@it.jgec.ac.in>
2024-04-15 00:56:05 +0530

    Status

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -9,7 +9,15 @@ import {
   AiOutlineUser,
   AiOutlineEye,
 } from "react-icons/ai";
-function Postcard({ $id, title, content, feature_img, userId, username }) {
+function Postcard({
+  $id,
+  title,
+  content,
+  status,
+  feature_img,
+  userId,
+  username,
+}) {
   const navigate = useNavigate();
   const parsedContent = parse(content);
 
@@ -24,65 +32,76 @@ function Postcard({ $id, title, content, feature_img, userId, username }) {
     }
   }
 
-  return (
-    <div className="bg-white rounded-xl w-full  flex justify-start items-start md:flex-row flex-col gap-6 p-4">
-      <Link to={`/post/${$id}`} className="md:max-w-[20vw] w-full">
-        <div className="w-full group">
-          <img
-            src={
-              feature_img
-                ? service.getFile(feature_img)
-                : "https://images.pexels.com/photos/372748/pexels-photo-372748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            }
-            alt={title}
-            className="  w-full group aspect-square object-cover rounded-xl"
-          />
-        </div>
-      </Link>
-      <div className=" md:h-[20vw] flex flex-col justify-between">
-        <div className="flex flex-col   text-start">
-          <h2 className="text-xl font-bold text-blue">{title}</h2>
-          {/* Display the text content of the first <p> tag */}
-          <div className="text-md text-gray text-wrap ">
-            {firstPTagText.substring(0, 200) ||
-              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi temporibus ipsam illo optio alias incidunt doloribus quia quae vero hic fugiat nesciunt, sunt illum, facilis aliquid maxime nam quibusdam magnam?"}
-            ...
-            <span
-              className="text-blue cursor-pointer"
-              onClick={() => {
-                navigate(`/post/${$id}`);
-              }}
-            >
-              read more
-            </span>
+  return status === "active" || userId === usedata.$id ? (
+    <>
+      <div
+        className={`${
+          status === "active" ? "bg-white" : "bg-slate-300"
+        } rounded-xl w-full  text-center   flex justify-start items-start md:flex-row flex-col gap-6 p-4`}
+      >
+        {status === "inactive" && (
+          <h1 className="text-gray font-bold absolute bg-slate-300 rounded-br-lg p-2 font-mono border-b-2 shadow-xl">
+            Inactive post
+          </h1>
+        )}
+        <Link to={`/post/${$id}`} className="md:max-w-[20vw] w-full">
+          <div className="w-full group">
+            <img
+              src={
+                feature_img
+                  ? service.getFile(feature_img)
+                  : "https://images.pexels.com/photos/372748/pexels-photo-372748.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              }
+              alt={title}
+              className="  w-full group aspect-square object-cover rounded-xl"
+            />
           </div>
-        </div>
-        <div className="flex flex-row   justify-between ">
-          <div>
-            <button className="flex flex-col  gap-2 bg-slate-300 p-2 rounded-md shadow-lg ">
-              <div className="flex justify-center items-center gap-2 ">
-                <AiOutlineUser className="border-2 border-blue text-blue rounded-full text-xl" />
+        </Link>
+        <div className=" md:h-[20vw] flex flex-col justify-between">
+          <div className="flex flex-col   text-start">
+            <h2 className="text-xl font-bold text-blue">{title}</h2>
+            {/* Display the text content of the first <p> tag */}
+            <div className="text-md text-gray text-wrap ">
+              {firstPTagText.substring(0, 200) ||
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi temporibus ipsam illo optio alias incidunt doloribus quia quae vero hic fugiat nesciunt, sunt illum, facilis aliquid maxime nam quibusdam magnam?"}
+              ...
+              <span
+                className="text-blue cursor-pointer"
+                onClick={() => {
+                  navigate(`/post/${$id}`);
+                }}
+              >
+                read more
+              </span>
+            </div>
+          </div>
+          <div className="flex flex-row   justify-between ">
+            <div>
+              <button className="flex flex-col  gap-2 bg-slate-300 p-2 rounded-md shadow-lg ">
+                <div className="flex justify-center items-center gap-2 ">
+                  <AiOutlineUser className="border-2 border-blue text-blue rounded-full text-xl" />
 
-                <p className="text-blue text-xs">{username || "Anonymus"}</p>
-              </div>
-            </button>
-          </div>
-          <div className="basis-1/3  flex gap-2 justify-around ">
-            <button className="btn text-xl bg-slate-300 p-2 rounded-md text-blue shadow-lg h-10 w-15 flex  items-center ">
-              <AiOutlineLike />
-              <p className="text-xs "> {usedata.like || 0}</p>
-            </button>
-            <button className="btn text-xl bg-slate-300 p-2 rounded-md text-blue shadow-lg h-10 w-10 grid place-content-center">
-              <AiOutlineComment />
-            </button>
-            <button className="btn text-xl bg-slate-300 p-2 rounded-md text-blue shadow-lg h-10 w-10 grid place-content-center">
-              <AiOutlineEye />
-            </button>
+                  <p className="text-blue text-xs">{username || "Anonymus"}</p>
+                </div>
+              </button>
+            </div>
+            <div className="basis-1/3  flex gap-2 justify-around ">
+              <button className="btn text-xl bg-slate-300 p-2 rounded-md text-blue shadow-lg h-10 w-15 flex  items-center ">
+                <AiOutlineLike />
+                <p className="text-xs "> {usedata.like || 0}</p>
+              </button>
+              <button className="btn text-xl bg-slate-300 p-2 rounded-md text-blue shadow-lg h-10 w-10 grid place-content-center">
+                <AiOutlineComment />
+              </button>
+              <button className="btn text-xl bg-slate-300 p-2 rounded-md text-blue shadow-lg h-10 w-10 grid place-content-center">
+                <AiOutlineEye />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    </>
+  ) : null;
 }
 
 export default Postcard;
